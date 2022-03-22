e8ba1b6ce72f19c39d357f8ffe2f70631ccff025
Jason Beres <jasonberes@gmail.com>
2022-03-22 14:17:18 -0400

    Customers Swagger demo

---
+++ Data/NorthwindCloudApp1Apicustomers.cs
@@ -0,0 +1,17 @@
+//namespace EmployeesRESTAPI.Data.NorthwindCloudApp1; // Razor won't recognize third level namespace
+namespace EmployeesRESTAPI.NorthwindCloudApp1;
+
+public class ApiCustomersType
+{
+    public string CustomerID { get; set; }
+    public string CompanyName { get; set; }
+    public string ContactName { get; set; }
+    public string ContactTitle { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string Region { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+    public string Fax { get; set; }
+}
